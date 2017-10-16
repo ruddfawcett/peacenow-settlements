51db8b77d153212a4aee7cbf730b50b2e686eeb3
Rudd Fawcett <rudd.fawcett@gmail.com>
2017-10-16 10:28:41 -0400

    Initial commit

---
+++ generate.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import pandas as pd
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+SETTLEMENT_URL = 'http://peacenow.org.il/en/settlements-watch/israeli-settlements-at-the-west-bank-the-list'
+
+def scrape():
+    req = Request(SETTLEMENT_URL, headers=HEADERS)
+    page = urlopen(req)
+    soup = BeautifulSoup(page, 'lxml')
+
+    listing = soup.find(id='settlements-list')
+
+    settlements = []
+
+    for item in listing.findAll('a'):
+        link = item['href']
+
+        req = Request(link, headers=HEADERS)
+        page = urlopen(req)
+        soup = BeautifulSoup(page, 'lxml')
+
+        container = soup.find('article')
+        name = container.find(class_='page-title').text
+        location = container.find(id='settleLocation')['location-data']
+
+        body = container.find('section', class_='entry-content cf')
+        type_ = body.find('span', class_='right natun').text
+
+        if (type_.replace(' ', '').lower() != 'settlement'):
+            continue
+
+        settlement = {
+            'name': name,
+            'longitude': float(location.split(' ')[0]),
+            'latitude': float(location.split(' ')[1])
+        }
+
+        settlements.append(settlement)
+
+    return settlements
+
+all_settlements = scrape()
+
+df = pd.DataFrame(all_settlements)
+df.to_csv('nowpeace-settlements.csv', index=False)
